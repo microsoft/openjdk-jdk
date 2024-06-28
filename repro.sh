64e7e7bbd31620acc783f49a63850573b805c5b5
John T <disoares@microsoft.com>
2024-06-28 14:51:40 -0400

    More graphs

--- repro.sh
+++ repro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OPTS="-XX:+UnlockDiagnosticVMOptions -XX:-UseCompressedOops -XX:+TraceReduceAllocationMerges"
+OPTS="-XX:+UnlockDiagnosticVMOptions -XX:-UseCompressedOops -XX:-TieredCompilation -XX:+TraceReduceAllocationMerges"
 
 # move all previous "/tmp/old*" folders to "/tmp/old.old*"
 for d in `ls -d /tmp/old* 2>/dev/null` ; do
@@ -21,4 +21,4 @@ mv -f /tmp/graph_* /tmp/old/ 2>/dev/null
 # make test CONF=release TEST=test/langtools/:repro JTREG="OPTIONS=--max-pool-size=5;JOBS=5;TIMEOUT_FACTOR=100;JAVA_OPTIONS=${OPTS};REPEAT_COUNT=50"
 
 # EXECUTE WHOLE GROUP
-  make test CONF=release TEST=test/langtools/:tier1 JTREG="JAVA_OPTIONS=${OPTS};REPEAT_COUNT=50"
+  make test CONF=release TEST=test/langtools/:tier1 JTREG="JAVA_OPTIONS=${OPTS};REPEAT_COUNT=500"
