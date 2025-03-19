swesonga/jdk

Reverse location of young and tenured generations

src/hotspot/share/gc/serial/serial_globals.hpp
@@ -35,6 +35,10 @@
           "When disabled, informs the GC to shrink the java heap directly"  \
           " to the target size at the next full GC rather than requiring"   \
           " smaller steps during multiple full GCs.")                       \
+                                                                            \
+  product(bool, SwapGenerations, false, EXPERIMENTAL,                       \
+          "When enabled, informs the GC to place the tenured region before" \
+          " the young region in memory.")                                   \
 
 // end of GC_SERIAL_FLAGS
 

microsoft/openjdk-jdk

src/hotspot/share/gc/serial/serial_globals.hpp
@@ -35,6 +35,10 @@
           "When disabled, informs the GC to shrink the java heap directly"  \
           " to the target size at the next full GC rather than requiring"   \
           " smaller steps during multiple full GCs.")                       \
+                                                                            \
+  product(bool, SwapSerialGCGenerations, false, EXPERIMENTAL,               \
+          "When enabled, informs the GC to place the tenured region before" \
+          " the young region in memory.")                                   \
 
 // end of GC_SERIAL_FLAGS
 